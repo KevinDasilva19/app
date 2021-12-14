3fb3abdc1d086a629ccefa851474b68bd037d3ea
KevinDasilva19 <kevin14ds@gmail.com>
2021-12-14 20:43:20 -0300

    desafioClase5

---
+++ src/components/ItemCount.js
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+function ItemCount ({stock, initial, onAdd}) {
+    
+    const [contador, setContador] = useState(initial)
+    console.log(contador, setContador)
+
+    const sumar = () => {
+        if (contador < stock) {
+            setContador (contador + 1)
+        } else {
+            console.log("No hay mas stock disponible")
+        }
+    }
+
+    const restar = () => {
+        if (contador > 0) {
+            setContador(contador - 1)
+        } else {
+            console.log("Ya no tienes productos en el carrito de compras")
+        }
+    }
+    
+    return (
+        <>
+            
+            <p>Carrito</p>
+            <p>{contador}</p>
+            <button onClick={sumar}>+</button>
+            <button onClick={onAdd}>Agregar a Carrito</button>
+            <button onClick={restar}>-</button>
+        </>
+    )
+}
+
+export default ItemCount 
